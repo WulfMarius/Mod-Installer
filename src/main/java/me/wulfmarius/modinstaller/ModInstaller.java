31f84cc013379f26bd40741385508ec92d6e8325
WulfMarius <WulfMarius@users.noreply.github.com>
2018-04-25 07:32:16 +0200

    updated version

--- src/main/java/me/wulfmarius/modinstaller/ModInstaller.java
+++ src/main/java/me/wulfmarius/modinstaller/ModInstaller.java
@@ -17,7 +17,7 @@ import me.wulfmarius.modinstaller.utils.JsonUtils;
 
 public class ModInstaller {
 
-    public static final String VERSION = "0.2.4";
+    public static final String VERSION = "0.2.5";
 
     private final Path basePath;
     private final Repository repository;
